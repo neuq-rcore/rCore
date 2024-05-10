4ab9928a6ba05f9898696dc7d07c05b3a1577a62
Caiyi Hsu <cai1hsu@outlook.com>
2024-05-10 14:12:08 +0800

    chore: Add IS_CI_ENVIRONMENT flag to enable conditional behavior in shutdown function

--- os/src/sbi/system.rs
+++ os/src/sbi/system.rs
@@ -1,13 +1,27 @@
+use sbi_rt::{system_reset, Shutdown, SystemFailure, NoReason};
+
 pub fn set_timer(timer: usize) {
     sbi_rt::set_timer(timer as _);
 }
 
-
 pub fn shutdown(failure: bool) -> ! {
     use super::qemu::{QEMUExit, QEMU_EXIT_HANDLE};
     
-    match failure {
-        true => QEMU_EXIT_HANDLE.exit_failure(),
-        false => QEMU_EXIT_HANDLE.exit_success(),
+    match option_env!("IS_CI_ENVIRONMENT") {
+        Some(_) => {
+            // QEMU_EXIT can mark a CI workflow as failed or successful
+            match failure {
+                true => QEMU_EXIT_HANDLE.exit_failure(),
+                false => QEMU_EXIT_HANDLE.exit_success(),
+            }
+        },
+        None => {
+            match failure {
+                true => system_reset(Shutdown, SystemFailure),
+                false => system_reset(Shutdown, NoReason),
+            };
+
+            unreachable!();
+        }
     }
 }
