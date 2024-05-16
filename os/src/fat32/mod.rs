fa1cccc56cac9dbd6a892fc5a2a569d2da275a18
Caiyi Hsu <cai1hsu@outlook.com>
2024-05-16 20:37:47 +0800

    feat: Add read_exact method to Fat32IO for reading exact buffer size

--- os/src/fat32/mod.rs
+++ os/src/fat32/mod.rs
@@ -49,17 +49,26 @@ impl IoBase for Fat32IO {
     type Error = ();
 }
 
-impl Read for Fat32IO {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+impl Fat32IO {
+    fn read_inner(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+        let len = buf.len();
+
+        assert!(
+            len <= 512,
+            "buf.len() must be less than or equal to 512, found: {}",
+            len
+        );
+
         let device = &mut self.device;
         let device_offset = device.get_position() % 512;
 
-        let size_read = if device_offset != 0 || buf.len() < 512 {
+        // Virtio_driver can only read 512 bytes at a time
+        let size_read = if device_offset != 0 || len < 512 {
             let mut tmp = [0u8; 512];
             device.read_blocks(&mut tmp);
 
             let start = device_offset;
-            let end = (device_offset + buf.len()).min(512);
+            let end = (device_offset + len).min(512);
 
             buf[..end - start].copy_from_slice(&tmp[start..end]);
             end - start
@@ -70,6 +79,34 @@ impl Read for Fat32IO {
 
         device.move_cursor(size_read);
         Ok(size_read)
+    }
+}
+
+impl Read for Fat32IO {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+        self.read_exact(buf).map(|_| buf.len()).map_err(|_| ())
+    }
+
+    fn read_exact(&mut self, mut buf: &mut [u8]) -> Result<(), Self::Error> {
+        while !buf.is_empty() {
+            match buf.len() {
+                0..=512 => {
+                    let size = self.read_inner(buf)?;
+                    buf = &mut buf[size..];
+                }
+                _ => {
+                    let (left, right) = buf.split_at_mut(512);
+                    self.read_inner(left)?;
+                    buf = right;
+                }
+            }
+        }
+        if buf.is_empty() {
+            Ok(())
+        } else {
+            log::debug!("failed to fill whole buffer in read_exact");
+            Err(())
+        }
     }
 }
 
