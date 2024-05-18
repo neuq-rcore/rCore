c3df362a91d2d6e8248cb8c285a5f64b4cfb4260
Caiyi Hsu <cai1hsu@outlook.com>
2024-05-18 23:01:15 +0800

    enlarge APP_COUNT

--- os/src/config.rs
+++ os/src/config.rs
@@ -6,7 +6,7 @@ pub const KERNEL_HEAP_SIZE: usize = 0x0080_0000; // 8MB
 pub const PAGE_SIZE: usize = 0x1000; // 4KB
 pub const PAGE_SIZE_BITS_WIDTH: usize = 12;
 
-pub const MAX_APP_NUM: usize = 4;
+pub const MAX_APP_NUM: usize = 8;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
