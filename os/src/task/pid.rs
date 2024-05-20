2074d4ce9bea254d66d223339ba5c014550f7fa3
Caiyi Hsu <cai1hsu@outlook.com>
2024-05-20 19:26:16 +0800

    add force_gc and test coverage

--- os/src/task/pid.rs
+++ os/src/task/pid.rs
@@ -68,6 +68,42 @@ impl PidManager {
 
         assert!(inserted, "pid already deallocated");
     }
+
+    // Call this when there are no processes left
+    pub fn try_gc_recycled(&mut self) {
+        // When there is no pid allocated, we can reset the current pid to 1
+
+        // Check that if all pids in self.recycled are continous,
+        // and the last one is self.current - 1
+
+        let mut iter = self.recycled.iter();
+
+        match iter.next() {
+            None => return, // fast path
+            Some(&pid) => {
+                let mut curr = pid;
+
+                while let Some(&pid) = iter.next() {
+                    if pid != curr + 1 {
+                        return;
+                    }
+
+                    curr = pid;
+                }
+
+                // We could gc the recycled pids as all of them are deallocated.
+                if curr == self.current - 1 {
+                    // it's actually safe to gc the recycled pids
+                    self.force_reset();
+                }
+            }
+        }
+    }
+
+    fn force_reset(&mut self) {
+        self.current = 1; // 0 is reserved for internal use
+        self.recycled.clear();
+    }
 }
 
 pub mod tests {
@@ -79,10 +115,15 @@ pub mod tests {
         test_deallocate_pid();
         test_allocate_after_deallocate_last_current();
         test_reallocate_deallocated_pid();
+        test_gc();
+
+        // This test should panic so we run it last
         test_deallocate_invalid_pid();
     }
 
     pub fn test_allocate_pid() {
+        PID_ALLOCATOR.exclusive_access().force_reset();
+
         debug!("Running `test_allocate_pid`");
         let pid1 = super::pid_alloc();
         assert_eq!(pid1.0, 1);
@@ -92,6 +133,8 @@ pub mod tests {
     }
 
     pub fn test_deallocate_pid() {
+        PID_ALLOCATOR.exclusive_access().force_reset();
+
         debug!("Running `test_deallocate_pid`");
         let pid1 = super::pid_alloc();
         let pid2 = super::pid_alloc();
@@ -100,6 +143,8 @@ pub mod tests {
     }
 
     pub fn test_reallocate_deallocated_pid() {
+        PID_ALLOCATOR.exclusive_access().force_reset();
+
         debug!("Running `test_reallocate_deallocated_pid`");
         let pid1 = super::pid_alloc();
         let pid2 = super::pid_alloc();
@@ -114,6 +159,8 @@ pub mod tests {
     }
 
     pub fn test_allocate_after_deallocate_last_current() {
+        PID_ALLOCATOR.exclusive_access().force_reset();
+
         debug!("Running `test_allocate_after_deallocate_last_current`");
         let _pid1 = super::pid_alloc();
         let pid2 = super::pid_alloc();
@@ -123,8 +170,35 @@ pub mod tests {
         assert_eq!(pid3.0, 2);
     }
 
+    pub fn test_gc() {
+        let mut allocator = PID_ALLOCATOR.exclusive_access();
+
+        allocator.force_reset();
+
+        {
+            let _p1 = super::pid_alloc();
+            let _p2 = super::pid_alloc();
+            
+            assert_eq!(_p2.0, 2);
+
+            // drop pids
+        }
+
+        let _p3 = super::pid_alloc();
+        assert_eq!(_p3.0, 3);
+
+        drop(_p3);
+
+        allocator.try_gc_recycled();
+
+        assert!(allocator.recycled.is_empty());
+        assert_eq!(allocator.current, 1);
+    }
+
     #[should_panic]
     pub fn test_deallocate_invalid_pid() {
+        PID_ALLOCATOR.exclusive_access().force_reset();
+
         debug!("Running `test_deallocate_invalid_pid`");
         let pid1 = PidHandle(100);
 
