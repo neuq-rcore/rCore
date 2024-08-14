23e65c6f8fe5bf09ea5b0068dd2981ff5dfc1fc2
Caiyi Shyu <cai1hsu@outlook.com>
2024-08-14 15:53:47 +0800

    temporarily disable vf2-sd

--- os/src/ext4/vf2.rs
+++ os/src/ext4/vf2.rs
@@ -2,6 +2,6 @@ use alloc::boxed::Box;
 
 use crate::{boards::{get_board, IBoard}, sync::UPSafeCell};
 
-use visionfive2_sd::{SDIo, SleepOps, Vf2SdDriver};
+// use visionfive2_sd::{SDIo, SleepOps, Vf2SdDriver};
 
 // TODO
