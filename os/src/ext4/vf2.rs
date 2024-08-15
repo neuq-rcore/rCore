03133b21759db7d82c16ec0fa7bdad26f842b4d0
Caiyi Shyu <cai1hsu@outlook.com>
2024-08-15 08:11:13 +0000

    code format

--- os/src/ext4/vf2.rs
+++ os/src/ext4/vf2.rs
@@ -1,6 +1,9 @@
 use alloc::boxed::Box;
 
-use crate::{boards::{get_board, IBoard}, sync::UPSafeCell};
+use crate::{
+    boards::{get_board, IBoard},
+    sync::UPSafeCell,
+};
 
 // use visionfive2_sd::{SDIo, SleepOps, Vf2SdDriver};
 
